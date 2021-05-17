669b9f69a2b893b179fb848177ab967fb7b0fdc6
Daniel Müller <acct.d-mueller@mailbox.org>
2021-05-18 00:27:46 +0200

    Vorbereitung für treffen mit Schlüter

--- prognose/MLForecast/visualize.py
+++ prognose/MLForecast/visualize.py
@@ -1,8 +1,26 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import seaborn as sns
 import numpy as np
 import pandas as pd
 
+
+sns.set_style("whitegrid")
+sns.set_context("paper")
+COLOR = mcolors.CSS4_COLORS["lightsteelblue"]
+FIGSIZE = (16,9)
+DPI = 600
+
+def footnote(axes, note):
+    axes.annotate(
+        note, 
+        xy = (1.0, -0.1),
+        xycoords='axes fraction',
+        ha='right',
+        va="center", 
+        fontsize=8
+    )
+    
 def plot_input_label_example(input_df, label_df):
     fig, axes = plt.subplots(2, 1, sharex=True)
     
@@ -17,7 +35,7 @@ def plot_input_label_example(input_df, label_df):
     plt.show()
     
 def plot_input_label_prediction_example(model, input_df, label_df):
-    fig, axes = plt.subplots(2, 1, sharex=True)
+    fig, axes = plt.subplots(2, 1, sharex=True, dpi=DPI, figsize=FIGSIZE)
     
     sns.lineplot(ax=axes[0], data=input_df, markers=True, dashes=False)
     axes[0].set_title("INPUT")
@@ -37,24 +55,24 @@ def plot_input_label_prediction_example(model, input_df, label_df):
     
     plt.show()  
     
-def boxplot(dfs, subtitles, xtitle, ytitle):
+def boxplot(dfs, suptitle, subtitles, xtitle, ytitle, note=None):
     assert type(dfs) == list
     
     if len(dfs) != 1:
-        fig, axes = plt.subplots(1, len(dfs), sharey=True)
-        axes[0].set_xlabel(xtitle)
+        fig, axes = plt.subplots(1, len(dfs), sharey=True, dpi=DPI, figsize=FIGSIZE)
+        axes[0].set_ylabel(ytitle)
+        fig.suptitle(suptitle)
         for i in range(len(dfs)):
-            axes[i].grid(axis="y", which="major")
-            axes[i].set_axisbelow(True)
-            sns.boxplot(ax=axes[i], data=dfs[i])
+            sns.boxplot(ax=axes[i], data=dfs[i], color=COLOR)
             axes[i].set_title(subtitles[i])
-            axes[i].set_ylabel(ytitle)
+            axes[i].set_xlabel(xtitle)
+        if note:
+            footnote(axes[-1], note)
             
     else:
-        plt.grid(axis="y", which="major")
-        plt.set_axisbelow(True)
-        plt.xlabel(xtitle)
+        raise("IMPLEMENT: dpi, figsize and footnote")
         sns.boxplot(data=dfs[0])
+        plt.xlabel(xtitle)
         plt.title(subtitles[0])
         plt.ylabel(ytitle)
         
