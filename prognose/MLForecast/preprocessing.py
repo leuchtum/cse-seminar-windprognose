dc1dc4dd12faeaa41aee7d8e3415070762be1c46
Daniel Müller <acct.d-mueller@mailbox.org>
2021-05-26 14:46:38 +0200

    began with pol2car_visualize

--- prognose/MLForecast/preprocessing.py
+++ prognose/MLForecast/preprocessing.py
@@ -2,20 +2,6 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
-def load_merged_dataframe(path, filenames):
-    """Load and merge DataFrames form pickle.
-
-    Args:
-        path (pathlib.Path): Directory containing the Data Pickles.
-        filenames (list): List containing the filenames that will be loaded and merged.
-
-    Returns:
-        pd.DataFrame: Merged DataFrame
-    """
-
-    dfs = [pd.read_pickle(path / f) for f in filenames]
-    return pd.concat(dfs, axis=1)
-
 
 def interpolate_columnwise(df: pd.DataFrame, max_gap=3) -> pd.DataFrame:
     """Fill missing data columnwise by interpolating with the last available data.
@@ -83,7 +69,7 @@ def pol2cart(df, speed="WS", direction="WD", x="WX", y="WY"):
                 s = key
             elif direction in key:
                 d = key
-                    
+
         wind = df[[s, d]]
         wind = wind.dropna()
 
@@ -96,17 +82,17 @@ def pol2cart(df, speed="WS", direction="WD", x="WX", y="WY"):
 
         appendix = ws.name.replace(speed, "")
         assert appendix == wd.name.replace(direction, "")
-        
+
         wx.name = x + appendix
         wy.name = y + appendix
 
         df = pd.concat([wx, wy, df], axis=1)
-        df = df.drop([d,s], axis=1)
+        df = df.drop([d, s], axis=1)
 
     return df
 
 
-def split_test_val_train(x,y, split):
+def split_test_val_train(x, y, split):
     assert sum(split) == 1
     x_test = x[:int(len(x)*split[0])]
     y_test = y[:int(len(y)*split[0])]
@@ -116,29 +102,29 @@ def split_test_val_train(x,y, split):
     y_train = y[int(len(x)*split[1]):]
     return (x_test, y_test), (x_val, y_val), (x_train, y_train)
 
-        
+
 def split_at_gaps(df, windowsize):
     not_nan = df.notna()
     not_nan = not_nan.all(axis=1)
     not_nan.index = range(len(not_nan))
-    
+
     true_idx = not_nan[not_nan].index
-    
+
     edges = [true_idx[0]]
-    
+
     for i in range(len(true_idx) - 1):
         idx = true_idx[i]
         next_idx = true_idx[i+1]
         if next_idx - idx != 1:
             edges.extend([idx, next_idx])
-            
+
     edges.append(true_idx[-1])
 
     intervals = []
-    for i in range(0,len(edges),2):
+    for i in range(0, len(edges), 2):
         if edges[i + 1] - edges[i] >= windowsize:
             intervals.append((edges[i], edges[i + 1]))
-            
+
     return intervals
 
 
@@ -146,47 +132,49 @@ def sample_sequences(pre_sampling_x, pre_sampling_y, x_width, y_width, shift):
     window_size = max(x_width, y_width + shift)
     assert len(pre_sampling_x) == len(pre_sampling_y)
     intervals = split_at_gaps(pre_sampling_x, window_size)
-    
+
     # Rename
-    pre_sampling_x = pre_sampling_x.rename(columns={key:f"INPUT_{key}" for key in pre_sampling_x.keys()})
-    pre_sampling_y = pre_sampling_y.rename(columns={key:f"LABEL_{key}" for key in pre_sampling_y.keys()})
-    
+    pre_sampling_x = pre_sampling_x.rename(
+        columns={key: f"INPUT_{key}" for key in pre_sampling_x.keys()})
+    pre_sampling_y = pre_sampling_y.rename(
+        columns={key: f"LABEL_{key}" for key in pre_sampling_y.keys()})
+
     # Sample
     x_samples = []
     y_samples = []
-    
+
     for inter in tqdm(intervals):
         for i in tqdm(range(inter[0], inter[1] - window_size)):
             x_samples.append(pre_sampling_x.iloc[i:i+x_width])
             y_samples.append(pre_sampling_y.iloc[i+shift:i+shift+y_width])
 
-        
     return x_samples, y_samples
 
 
 def df_tuple_to_np(df_tuple):
-    convert = lambda df_list: np.array([d.values for d in df_list])
+    def convert(df_list): return np.array([d.values for d in df_list])
     return tuple([convert(df_list) for df_list in df_tuple])
-
 
 
 class DataBaseReport:
     def __init__(self, pre_sampling_x, pre_sampling_y, post_sampling_x, post_sampling_y, shift, split):
         # Pre sampling
         self.x_names = pre_sampling_x.keys().to_list()
-        self.x_stats = pre_sampling_x.describe().loc[["count", "min", "max", "std"]].to_dict()
+        self.x_stats = pre_sampling_x.describe(
+        ).loc[["count", "min", "max", "std"]].to_dict()
         self.y_names = pre_sampling_y.keys().to_list()
-        self.y_stats = pre_sampling_y.describe().loc[["count", "min", "max", "std"]].to_dict()
+        self.y_stats = pre_sampling_y.describe(
+        ).loc[["count", "min", "max", "std"]].to_dict()
         self.start = str(pre_sampling_x.index[0])
         self.end = str(pre_sampling_x.index[-1])
         self.n_measurements = len(pre_sampling_x)
-        
+
         # Post sampling
         self.n_samples = len(post_sampling_x)
         self.x_shape = post_sampling_x[0].shape
         self.y_shape = post_sampling_y[0].shape
         self.split = split
         self.shift = shift
-        
+
     def report(self):
         return self.__dict__
